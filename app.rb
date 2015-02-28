cc0f2c2ffeea6f0dedbc0d857b6afefea826acf0
Shuyang Li <shuyang.li.95@gmail.com>
2015-02-28 16:21:45 -0600

    People list

--- app.rb
+++ app.rb
@@ -1,6 +1,5 @@
 require 'sinatra'
 require 'sinatra/activerecord'
-require 'pry'
 
 require_relative 'models/init'
 
@@ -15,20 +14,50 @@ class NDTechNetworkApp < Sinatra::Base
   enable :sessions
 
   get '/' do
-    @people = Person.all
-    erb :main_layout, :layout => false do
-      erb :hello
+    @experiences = Experience.all
+    erb :people_list, :layout => false do
+      erb :layout
     end
   end
 
-  get '/make' do
-    Person.create(
-      :firstname => "Shuyang",
-      :lastname => "Li",
-      :graduation_class => "2016",
-      :emailnonnd => "shuyang.li.95@gmail.com"
-    )
-    "Hello world again"
+  post '/person' do
+    if param[:id]
+      p = Person.find(param[:id])
+      p.firstname = param[:firstname]
+      p.lastname = param[:lastname]
+      p.emailnd = param[:emailnd]
+      p.emailnonnd = param[:emailnonnd]
+      p.graduation_class = param[:graduation_class]
+      p.currentcity = param[:currentcity]
+      p.save
+    else
+      Person.create(:firstname => param[:firstname],
+                    :lastname => param[:lastname],
+                    :emailnd => param[:emailnd],
+                    :emailnonnd => param[:emailnonnd],
+                    :graduation_class => param[:graduation_class],
+                    :currentcity => param[:currentcity])
+    end
+  end
+
+  post '/experience' do
+    if param[:id]
+      e = Experience.find(param[:id])
+      e.person_id = param[:person_id]
+      e.year = param[:graduation_class]
+      e.company = param[:company]
+      e.is_intern = param[:is_intern]
+      e.position_desc = param[:position_desc]
+      e.sentiment = param[:sentiment]
+      e.save
+    else
+      Experience.create(:person_id => param[:person_id],
+                        :year => param[:graduation_class],
+                        :company => param[:company],
+                        :is_intern => param[:is_intern],
+                        :position_desc => param[:position_desc],
+                        :sentiment => param[:sentiment])
+    end
   end
 
 end
--- models/person.rb
+++ models/person.rb
@@ -1,5 +1,3 @@
 class Person < ActiveRecord::Base
-  validates :firstname, :lastname, :class, :emailnonnd, presence: true
-  validates :emailnd, uniqueness: true
-  validates :emailnonnd, uniqueness: true
+  validates :emailnonnd, presence: true
 end
