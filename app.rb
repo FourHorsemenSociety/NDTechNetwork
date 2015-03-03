5bc93f2ca5c0535110a4a47d4de4ce28e522d6e8
Shuyang Li <shuyang.li.95@gmail.com>
2015-03-02 22:54:56 -0500

    Use first_or_create for more succinct code

--- app.rb
+++ app.rb
@@ -69,43 +69,23 @@ class NDTechNetworkApp < Sinatra::Base
   end
 
   post '/person' do
-    if param[:id]
-      p = Person.find(param[:id])
-      p.firstname = param[:firstname]
-      p.lastname = param[:lastname]
-      p.emailnd = param[:emailnd]
-      p.emailnonnd = param[:emailnonnd]
-      p.graduation_class = param[:graduation_class]
-      p.currentcity = param[:currentcity]
-      p.save
-    else
-      Person.create(:firstname => param[:firstname],
-                    :lastname => param[:lastname],
-                    :emailnd => param[:emailnd],
-                    :emailnonnd => param[:emailnonnd],
-                    :graduation_class => param[:graduation_class],
-                    :currentcity => param[:currentcity])
-    end
+    Person.where(:id => param[:id]).first_or_create(
+        :firstname => param[:firstname],
+        :lastname => param[:lastname],
+        :emailnd => param[:emailnd],
+        :emailnonnd => param[:emailnonnd],
+        :graduation_class => param[:graduation_class],
+        :currentcity => param[:currentcity])
   end
 
   post '/experience' do
-    if param[:id]
-      e = Experience.find(param[:id])
-      e.person_id = param[:person_id]
-      e.year = param[:graduation_class]
-      e.company = param[:company]
-      e.is_intern = param[:is_intern]
-      e.position_desc = param[:position_desc]
-      e.sentiment = param[:sentiment]
-      e.save
-    else
-      Experience.create(:person_id => param[:person_id],
-                        :year => param[:graduation_class],
-                        :company => param[:company],
-                        :is_intern => param[:is_intern],
-                        :position_desc => param[:position_desc],
-                        :sentiment => param[:sentiment])
-    end
+    Experience.where(:id => param[:id]).first_or_create(
+      :person_id => param[:person_id],
+      :year => param[:graduation_class],
+      :company => param[:company],
+      :is_intern => param[:is_intern],
+      :position_desc => param[:position_desc],
+      :sentiment => param[:sentiment])
   end
 
 end
